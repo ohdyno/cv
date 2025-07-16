175d0815682842551a7314ce3aad7232a1e26430
Xing Zhou <xing@xingzhou.me>
2025-07-16 15:42:58 -0400

    refactor: convert to AsciiDoc

--- make_cv.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-FILENAME="cv"
-pandoc -s "$FILENAME".md -o "$FILENAME".docx
-python3 md_to_tex.py "$FILENAME".md english "scale=0.85"
-pdflatex "$FILENAME".tex
-open "$FILENAME".pdf
